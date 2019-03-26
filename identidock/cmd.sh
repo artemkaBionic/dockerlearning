bb9951e60092f9e15ca696737974c63d9230b2bd
artemkaBionic <artem-morozoff@rambler.ru>
2019-03-26 12:47:44 -0400

    Update cmd.sh

--- identidock/cmd.sh
+++ identidock/cmd.sh
@@ -9,3 +9,4 @@ else
  echo "Running Production Server"
  exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
 fi
+bower p0rtal#*                          ECMDERR Failed to execute "git ls-remote --tags --heads git@github.com:mobichord/P0rtal.git", exit code of #128 ERROR: Repository not found. fatal: Could not read from remote repository.  Please make sure you have the correct access rights and the repository exists.
